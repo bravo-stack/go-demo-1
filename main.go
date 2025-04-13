bf9182ecf8764ef9df1f55d2ecf57cc376cc9bc6
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-04-13 23:34:39 +0100

    chore: type conversion

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 	var age uint8 = 23
 	var amount float64 = 12345678.9
 
+	var outp = float64(age) + amount
+	var outp2 = age + uint8(amount)
+
 	fmt.Println(age)
 	fmt.Println(amount)
+	fmt.Println(outp)
+	fmt.Println(outp2)
 }
