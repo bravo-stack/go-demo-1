46714023bc45868a6b12189818305a33e65d7a9c
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-04-13 23:31:18 +0100

    chore: basic types

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/bravo-stack/go-demo-1/mascot"
-	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(mascot.BestMascot())
-	fmt.Println(quote.Go())
+
+	var age uint8 = 23
+	var amount float64 = 12345678.9
+
+	fmt.Println(age)
+	fmt.Println(amount)
 }
