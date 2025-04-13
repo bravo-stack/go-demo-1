15760aea00f0f71b6c1519f96cd6eff99c86a359
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-04-13 23:47:05 +0100

    chore: boolean

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
 )
 
 func main() {
+	var isAwesome bool = false
+	var isCool bool = true
 
-	var str string = "Hello, ¥§World!"
-
-	fmt.Println(len(str))
-	fmt.Println(utf8.RuneCountInString(str))
-	
-	var myRune rune = 'g'
-	fmt.Println(myRune)
+	fmt.Println(isAwesome)
+	fmt.Println(isCool)
 }
