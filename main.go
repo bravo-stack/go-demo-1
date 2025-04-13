6f7feaff20c77b0143f9395dc9abdccc6aee7853
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-04-13 23:53:37 +0100

    chore: initializing variables

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+/*
+	default values for uint, uint8, uint16, uint32, uint64, int, int8, int16, int32, int64, float32, float64, rune is 0
+	default values for string is "" (empty string)
+	default values for bool is false
+*/
+
 package main
 
 import (
@@ -5,9 +11,12 @@ import (
 )
 
 func main() {
-	var isAwesome bool = false
-	var isCool bool = true
+	myVar := "Hello, World!"
 
-	fmt.Println(isAwesome)
-	fmt.Println(isCool)
+	fmt.Println(myVar)
+
+	// var var1, var2, var3 int = 1, 2, 3
+	// var var1, var2, var3 = 1, 2, 3
+	var1, var2, var3 := 56, 78, 90
+	fmt.Println(var1, var2, var3)
 }
